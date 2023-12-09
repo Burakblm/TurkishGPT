19001a29457ad7a4a21742ad26c76a1df07c1cc3
Burakblm <burakbulama5454@gmail.com>
2023-12-09 19:28:03 +0300

    data preparation code

--- train.py
+++ train.py
@@ -1,14 +1,22 @@
 import os
 import time
 import math
-import pickle
-
-from contextlib import nullcontext
 
 import torch
 import numpy as np
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
 
 from model import ModelArgs, Transformer
+from prepare_data import load_tensor, prepare_data
 
+device = "cpu"
+device_type = "cpu"
+
+batch_size = 32
+block_size = 128
+
+
+train_data = load_tensor("train")
+val_data = load_tensor("val")
+
+print(train_data.shape)
+print(val_data.shape)
