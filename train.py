570964a0d3849d0eff419446e253a7d5dde222fb
Burakblm <burakbulama5454@gmail.com>
2023-12-12 20:24:07 +0300

    CPU single GPU and DDP

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import time
 import math
 from dataclasses import dataclass
 from typing import Optional
+import random
 
 import torch
 from torch.nn import functional as F
@@ -24,6 +25,8 @@ from utils import get_tokenizer
 tokenizer = get_tokenizer()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+ddp = int(os.environ.get("RANK", -1)) != -1
+
 
 eval_iters = 10
 max_iters = 100
@@ -57,30 +60,46 @@ class TrainArgs:
 def ddp_setup():
     init_process_group(backend="nccl")
 
+def destroy_ddp():
+    destroy_process_group()
+
+
 class Trainer:
 
     def __init__(self,
                 model: Transformer,
-                train_data: Dataset,
+                train_data: DataLoader,
                 optimizer: torch.optim.Optimizer,
                 save_every: int,
                 snapshot_path: str,
+                ddp: bool = False,
+                eval_iters: int = 100,
+                device: Optional[str] = None,
                 args: Optional[TrainArgs] = None,
                 val_data: Optional[Dataset] = None,
                 ) -> None:
         
         self.args = args
-        self.gpu_id = int(os.environ["LOCAL_RANK"])
-        self.model = model.to(self.gpu_id)
+        self.ddp = ddp
+        self.device = device
+        self.eval_iters = eval_iters
+
+        if self.ddp:
+            self.gpu_id = int(os.environ["LOCAL_RANK"])
+            self.model = model.to(self.gpu_id)
+            self.model = DDP(self.model, device_ids=[self.gpu_id])
+
+        self.model = model.to(device)
+        self.epochs_run = 0
         self.train_data = train_data
         self.val_data = val_data
+        self.split_data = {"train": self.train_data,
+                           "val": self.val_data}
         self.optimizer = optimizer
         self.save_every = save_every
-        self.epochs_run = 0
         if os.path.exists(snapshot_path):
             print("Loading Snapshot")
             self._load_snapshot(snapshot_path)
-        self.model = DDP(self.model, device_ids=[self.gpu_id])
 
     def _load_snapshot(self, snapshot_path):
         snapshot = torch.load(snapshot_path)
@@ -96,23 +115,42 @@ class Trainer:
 
     def _run_epoch(self, epoch):
         bs = len(next(iter(self.train_data))[0])
-        print(f"[GPU:{self.gpu_id}] Epoch {epoch} | Batchsize: {bs} | Steps: {len(self.train_data)}")
-        for inputs, targets in self.train_data:
-            inputs = inputs.to(self.gpu_id)
-            targets = targets.to(self.gpu_id)
+        print(f"[GPU:{self.gpu_id if self.ddp else device}] Epoch {epoch} | Batchsize: {bs} | Steps: {len(self.train_data)}")
+        for i, (inputs, targets) in enumerate(self.train_data):
+            inputs = inputs.to(self.gpu_id if self.ddp else device)
+            targets = targets.to(self.gpu_id if self.ddp else device)
             self._run_batch(inputs, targets)
+            if i % eval_iters == 0:
+                out = self.calculate_loss()
+                print(f"Train loss: {out['train']:.4f}" + (f" | Val loss : {out['val']:.4f}" if self.val_data is not None else ""))
 
     def _save_snapshot(self, epoch):
         snapshot = {}
-        snapshot["MODEL_STATE"] = self.model.module.state_dict()
+        snapshot["MODEL_STATE"] = self.model.module.state_dict() if self.ddp else self.model.state_dict()
         snapshot["EPOCHS_RUN"] = epoch
-        #torch.save(snapshot, "snapshot.pt")
+        torch.save(snapshot, "snapshot.pt")
         print(f"Epoch {epoch} | training snapshot save at snapshot.pt")
-
+    
+    @torch.no_grad()
+    def calculate_loss(self):
+        out = {}
+        self.model.eval()
+        for i in ["train", "val"]:
+            if self.split_data[i] is not None:
+                losses = torch.zeros(self.eval_iters)
+                data = iter(self.split_data[i])
+                for j in range(self.eval_iters):
+                    inputs, targets = next(data)
+                    logits, loss = self.model(inputs, targets)
+                    losses[j] = loss.item()
+            out[i] = losses.mean()
+        self.model.train()
+        return out
+        
     def train(self, max_epochs: int):
         for epoch in range(self.epochs_run, max_epochs):
             self._run_epoch(epoch)
-            if self.gpu_id == 0 and epoch % self.save_every == 0:
+            if self.gpu_id if self.ddp else device == 0 and epoch % self.save_every == 0:
                 self._save_snapshot(epoch)
 
 
@@ -128,27 +166,39 @@ turkgptconfing = ModelArgs(**model_args)
 
 def load_train_objs():
     train_data = GPTDataset("train", batch_size=1, block_size=32)
+    val_data = GPTDataset("val", batch_size=1, block_size=32)
     model = Transformer(turkgptconfing)
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-    return train_data, model, optimizer
+    return train_data, val_data, model, optimizer
 
 def prepare_dataloader(dataset: Dataset, batch_size: int):
-    return DataLoader(
-        dataset,
-        batch_size=batch_size,
-        pin_memory=True,
-        shuffle=False,
-        sampler=DistributedSampler(dataset)
-    )
+    if ddp:
+        return DataLoader(
+            dataset,
+            batch_size=batch_size,
+            pin_memory=True,
+            shuffle=False,
+            sampler=DistributedSampler(dataset)
+        )
+    else:
+        return DataLoader(
+            dataset,
+            batch_size=batch_size,
+            pin_memory=True,
+            shuffle=False,
+            num_workers=0
+        )
 
 def main(total_epoch: int, save_every: int, snapshot_path: str = "snapshot.pt"):
-    ddp_setup()
-    dataset, model, optimizer = load_train_objs()
-    train_data = prepare_dataloader(dataset, batch_size=32)
-    trainer = Trainer(model=model, train_data=train_data, optimizer=optimizer, save_every=save_every, snapshot_path=snapshot_path)
+    if ddp:
+        ddp_setup()
+    train_data, val_data, model, optimizer = load_train_objs()
+    train_data = prepare_dataloader(train_data, batch_size=32)
+    val_data = prepare_dataloader(val_data, batch_size=32)
+    trainer = Trainer(model=model, train_data=train_data, optimizer=optimizer, ddp=ddp, save_every=save_every, snapshot_path=snapshot_path,eval_iters=eval_iters)
     trainer.train(total_epoch)
-    destroy_process_group()
-
+    if ddp:
+        destroy_ddp()
 
 if __name__ == "__main__":
     import sys
