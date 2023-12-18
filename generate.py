a16b65ea0096c50e07bb19e0b753d54494c1a6c7
BurakBlm <burakbulama5454@gmail.com>
2023-12-18 19:56:32 +0300

    generate file created

---
+++ generate.py
@@ -0,0 +1,16 @@
+import torch
+import os
+from utils import get_tokenizer
+
+
+path = os.getcwd() + "/model/snapshot.pt"
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+tokenizer = get_tokenizer()
+
+
+model = torch.load(path)
+
+idx = torch.zeros((1, 1), dtype=torch.long, device=device)
+res = model.generate(idx=idx, do_sample=True, top_k=10, temprature=0.8, max_new_tokens=500)[0].tolist()
+print(tokenizer.decode(res))
