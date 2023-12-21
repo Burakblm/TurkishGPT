ec372fcd459727d1d7fb16218dea8958a1631bb8
BurakBlm <burakbulama5454@gmail.com>
2023-12-21 12:57:13 +0300

    model args

--- generate.py
+++ generate.py
@@ -1,14 +1,31 @@
 import torch
 import os
 from utils import get_tokenizer
-from model import Transformer
+from model import Transformer, ModelArgs
 
 path = os.getcwd() + "/snapshot.pt"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-tokenizer = get_tokenizer()
+block_size = 1024
+vocab_size= 32000
+n_layer = 12
+n_head = 12
+n_embd= 768
+dropout = 0.0
+bias = False
 
-model = Transformer()
+tokenizer = get_tokenizer()
+model_args = dict(
+        block_size=block_size,
+        vocab_size=vocab_size,
+        n_layer=n_layer,
+        n_head=n_head,
+        dropout=dropout,
+        bias=bias,
+        n_embd=n_embd,
+    )
+turkgptconfing = ModelArgs(**model_args)
+model = Transformer(turkgptconfing)
 
 snapshot = torch.load(path)
 model.load_state_dict(snapshot["MODEL_STATE"])
