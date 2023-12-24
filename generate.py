fbfacb5c5b1cb5c173df591dff7dd22a4818ecbb
BurakBlm <burakbulama5454@gmail.com>
2023-12-24 11:18:37 +0300

    multiple gpu generation

--- generate.py
+++ generate.py
@@ -1,10 +1,23 @@
 import torch
+from torch.distributed import init_process_group, destroy_process_group
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+
 import os
+
 from utils import get_tokenizer
 from model import Transformer, ModelArgs
 
 path = os.getcwd() + "/model/snapshot.pt"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+dtype = "bfloat16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "flaot16"
+
+def ddp_setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12344"
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
+
 
 block_size = 1024
 vocab_size= 32000
@@ -30,11 +43,38 @@ model = Transformer(turkgptconfing)
 snapshot = torch.load(path)
 model.load_state_dict(snapshot["MODEL_STATE"])
 
-model.to(device)
+
+def generate_text(model):
+    model.eval()
+    idx = torch.zeros((1, 1), dtype=torch.long, device=device)
+    res = model.generate(idx=idx, do_sample=True, top_k=10, temprature=0.8, max_new_tokens=500)[0].tolist()
+    return tokenizer.decode(res)
+
+
+def main(rank: int, world_size: int):
+    if torch.cuda.is_available():
+        n_gpus = torch.cuda.device_count()
+    ddp_setup(rank=rank, world_size=world_size)
+    model.cuda(rank)
+
+    mp.spawn(run, args=(rank, world_size, n_gpus), nprocs=n_gpus)
+
+
+
+def run(rank, world_size):
+    if torch.cuda.is_available():
+        torch.cuda.set_device(rank)
+
+    ddp_setup(rank, world_size)
+    model = None
+
+    if torch.cuda.is_available():
+        model.cuda(rank)
+        model = DDP(model, device_ids=[rank])
+
+    gen_text = generate_text(model)
+
 
 if __name__ == "__main__":
-    import sys
-    max_new_token = int(sys.argv[1])
-    idx = torch.zeros((1, 1), dtype=torch.long, device=device)
-    res = model.generate(idx=idx, do_sample=True, top_k=10, temprature=0.8, max_new_tokens=max_new_token)[0].tolist()
-    print(tokenizer.decode(res))
+    world_size = torch.cuda.device_count() if torch.cuda.is_available() else 1
+    mp.spawn(run, args=(world_size,), nprocs=world_size)
